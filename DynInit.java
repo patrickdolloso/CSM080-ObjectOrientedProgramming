75082b37e6370485847635883fb29b51239f8346
PATRICK ROBERTS DOLLOSO <patrick.dolloso@gmail.com>
2023-01-26 14:19:47 +0000

    Added more java practice files

---
+++ DynInit.java
@@ -0,0 +1,16 @@
+// Demonstrate dynamic initiation
+class DynInit {
+  public static void main(String args[]) {
+    double radius = 4, height = 5;
+
+    // dynamically initialize volume
+    double volume = 3.1416 * radius * radius * height;
+    System.out.println("Vol is: " + volume);
+  }
+}
+
+/*
+- Here, 3 local variable radius, height, and volume are declared.
+- The first 
+
+*/
---
+++ StrDemo.java
@@ -0,0 +1,8 @@
+// Demo escape sequences in Strings
+class StrDemo {
+  public static void main(String args[]) {
+    System.out.println("Firstline \nSecondLine");
+    System.out.println("A\tB\tC");
+    System.out.println("D\tE\tF");
+  }
+}
